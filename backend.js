4f583d79c5bf9a56cd93513e831e39f7dec0a87b
SamuelNakab <167198088+SamuelNakab@users.noreply.github.com>
2024-09-10 08:47:34 -0300

    oouih9g

--- backend.js
+++ backend.js
@@ -1,19 +1,27 @@
 import fs from "fs";
-import {onEvent, startServer} from "soquetic";
+import {onEvent,startServer} from "soquetic";
+let pedidos = JSON.parse(fs.readFileSync('data/pedidos.json', 'utf8') || '[]');
 
-let sabores = JSON.parse(fs.readFileSync("data/sabores.json", "utf-8"));
-let productos = JSON.parse(fs.readFileSync("data/productos.json", "utf-8"));
-let pedidos = JSON.parse(fs.readFileSync("data/pedidos.json", "utf-8"));
 
-onEvent("sabores",() => {
-    return sabores;
-})
+function guardar_pedido(pedido){
+    pedidos.push(pedido);
+    fs.writeFileSync("data/pedidos.json",JSON.stringify(pedidos),null,2);
 
-onEvent("productos",() => {
+    return ({ok:true});
+}
+function poner_productos(){
+    let productos= JSON.parse(fs.readFileSync("data/productos.json"));
     return productos;
-})
+}
 
-onEvent("pedidos",(algo) => {
-    fs.writeFileSync("data/pedidos.json", JSON.stringify(algo));
-    return {ok : true};
-})
+
+function poner_sabores(){
+    let sabores= JSON.parse(fs.readFileSync("data/sabores.json"))
+    return sabores;
+}
+onEvent("sabores", poner_sabores)
+onEvent("productos", poner_productos)
+onEvent("pedido", guardar_pedido)
+
+
+startServer();
